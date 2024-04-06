be90ba87fe6b0085dfbdc3b8053999eaafeabc6a
ngure808 <mwangingure808@gmail.com>
2024-04-06 15:34:44 +0300

    list of movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const localURL = "http://localhost:3000/films"
+    const ul = document.getElementById("films")
+
+    fetch(localURL)
+    .then(response => response.json())
+    .then(films => 
+        films.forEach((value) => addsTitle(value.title.toUpperCase()))
+    )
+
+    function addsTitle(title){
+        const li = document.createElement("li")
+        li.innerHTML = title
+        ul.appendChild(li)
+        li.style.textDecoration = "underline"
+    }
+
+    // const image = document.getElementById("poster")
+    
+})
